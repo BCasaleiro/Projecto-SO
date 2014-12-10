b7f34fd6c6f6d14cb18e584653462cc407fc459f
Bernardo Casaleiro <bernardocasaleiro@gmail.com>
2014-12-10 23:05:57 +0000

    added process creation

--- server.c
+++ server.c
@@ -2,6 +2,38 @@
 
 int main (int argc, char const *argv[])
 {
-    
+
+    if(init()){
+
+    }
+
+    return 0;
+}
+
+int init()
+{
+    int i;
+
+    for(i = 0; i < N_PROC; i++) {
+        if( (id_proc[i] = fork()) == 0) {
+            switch(i){
+                case 0:
+                    //Config Manager
+
+                    break;
+                case 1:
+                    //Stat Manager
+                    break;
+                case 2:
+                    //Main
+
+                    break;
+                default:
+                    perror("While creating process\n");
+                    return 1;
+            }
+        }
+    }
+
     return 0;
 }
---
+++ server.h
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define N_PROC 3
+
+#define DEBUG
+
+pid_t id_proc[N_PROC];
+
+
+int init();
